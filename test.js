c03716320c6c4509d43cb2efa6f2e162f98c4c60
Yash-Kumar-tech <77187674+Yash-Kumar-tech@users.noreply.github.com>
2023-07-20 17:47:12 +0000

    Copilot wrote function to calculate factorial

--- test.js
+++ test.js
@@ -2,7 +2,10 @@ function calculateNumbers(var1, var2) {
     console.console.log(var1 + ", " + var2);
 }
 
-function factorial(var1) {
+function factorial(var1) {  
     //Function to calculate factorial of var1
-    
+    if (var1 === 0) {
+        return 1;
+    }   
+    return var1 * factorial(var1-1);    
 }
