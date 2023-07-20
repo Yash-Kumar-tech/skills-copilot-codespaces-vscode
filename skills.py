788e2561d0edf4a623d63672c1cd2f0ef0ebc670
Yash-Kumar-tech <77187674+Yash-Kumar-tech@users.noreply.github.com>
2023-07-20 17:41:55 +0000

    More filesgit add .

--- skills.py
+++ skills.py
@@ -1,2 +1,2 @@
 def calculateNumbers(num1, num2):
-    
+    print(f'num1 = ${num1}, num2 = ${num2}')
--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-
 function calculateNumbers(var1, var2) {
     console.console.log(var1 + ", " + var2);
+}
+
+function factorial(var1) {
+    //Function to calculate factorial of var1
+    
 }
