d274de85f3c4c49318696681f8b5c33921a8ccc6
Yash-Kumar-tech <77187674+Yash-Kumar-tech@users.noreply.github.com>
2023-07-20 17:53:03 +0000

    Copilot first commit

--- skills.py
+++ skills.py
@@ -1,2 +1,10 @@
 def calculateNumbers(num1, num2):
     print(f'num1 = ${num1}, num2 = ${num2}')
+
+def isOdd(num1):
+    #Function that checks if input number is odd or even
+    if num1 % 2 == 0:
+        print(f'{num1} is even')
+    else:
+        print(f'{num1} is odd')
+        
